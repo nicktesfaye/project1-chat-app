e41230733cfbd6f3c999d369fa59fb4a7a0bfff5
nicktesfaye <83650404+nicktesfaye@users.noreply.github.com>
2021-08-24 15:19:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import useLocalStorage from '../hooks/useLocalStorage'
+import Login from './Login'
+import Dashboard from './Dashboard'
+
+
+function App() {
+  const [id,setId] = useLocalStorage('id')
+
+  return (
+
+  id ? <Dashboard  id = {id }/> : <Login onIdSubmit={setId} /> 
+
+  )
+}
+
+export default App;
---
+++ Contacts.js
@@ -0,0 +1,11 @@
+import React  from 'react'
+
+
+
+export default function Contacts({id}){
+    return(
+        <div>
+                Contacts
+        </div>
+    )
+}
